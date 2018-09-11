24e08fcf047084791630dc095cd17af6f0b2c9d7
NaomiBid <naomi0bid@gmail.com>
2018-09-12 00:39:56 +0100

    print only names with less than 12 characters

--- directory.rb
+++ directory.rb
@@ -18,7 +18,7 @@ end
 
 def print(students)
   students.each do |student|
-    if student[:name][0] == "N"
+    if student[:name].length < 12
       puts "#{student[:name]} (#{student[:cohort]} cohort)"
     end
   end
