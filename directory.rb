243f973eec785d9b9e606fdfaf11e0004fa0b300
NaomiBid <naomi0bid@gmail.com>
2018-09-12 00:37:01 +0100

    print only names beginning with N

--- directory.rb
+++ directory.rb
@@ -17,8 +17,10 @@ def print_header
 end
 
 def print(students)
-  students.each_with_index do |student, index|
-    puts "#{index + 1} #{student[:name]} (#{student[:cohort]} cohort)"
+  students.each do |student|
+    if student[:name][0] == "N"
+      puts "#{student[:name]} (#{student[:cohort]} cohort)"
+    end
   end
 end
 
