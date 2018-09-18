9f5b4dc7f834531d542a50fe6064410c12bdc92d
NaomiBid <naomi0bid@gmail.com>
2018-09-18 23:31:47 +0100

    did some refactoring

--- directory.rb
+++ directory.rb
@@ -1,27 +1,6 @@
-def interactive_menu
-  students = []
-  loop do
-    puts "1. Input the students"
-    puts "2. Show the students"
-    puts "9. Exit"
-    selection = gets.chomp
-    case selection
-    when "1"
-      students = input_students
-    when "2"
-      print_header
-      print(students)
-      print_footer(students)
-    when "9"
-      exit
-    else
-      puts "I don't know what you meant, try again"
-    end
-  end
-end
+@students = []
 
 def input_students
-  students = []
   puts "Please enter the names of the students"
   puts "To finish, just hit return twice"
   name = gets.strip
@@ -34,11 +13,11 @@ def input_students
     cohort = "unknown"
   end
   while !name.empty? do
-    students << {name: name,cohort: cohort}
-    if students.count == 1
-      puts "Now we have #{students.count} student"
+    @students << {name: name,cohort: cohort}
+    if @students.count == 1
+      puts "Now we have #{@students.count} student"
     else
-      puts "Now we have #{students.count} students"
+      puts "Now we have #{@students.count} students"
     end
     name = gets.strip
     puts "Enter thier cohort"
@@ -47,7 +26,39 @@ def input_students
       cohort = "unknown"
     end
   end
-  students
+  @students
+end
+
+def interactive_menu
+  loop do
+    print_menu
+    process(gets.chomp)
+  end
+end
+
+def process(selection)
+  case selection
+    when "1"
+      input_students
+    when "2"
+      show_students
+    when "9"
+      exit
+    else
+      puts "I don't know what you mean, try again"
+  end
+end
+
+def print_menu
+  puts "1. Input the students"
+  puts "2. Show the students"
+  puts "9. Exit"
+end
+
+def show_students
+  print_header
+  print_student_list
+  print_footer
 end
 
 def print_header
@@ -55,25 +66,21 @@ def print_header
   puts "-------------"
 end
 
-def print(students)
+def print_student_list
   count = 0
-  until count == students.count
-    puts "#{students[count][:name]} (#{students[count][:cohort]} cohort)"
+  until count == @students.count
+    puts "#{@students[count][:name]} (#{@students[count][:cohort]} cohort)"
     count += 1
   end
 end
 
-def print_footer(names)
-  if names.count == 1
-    puts "Overall, we have #{names.count} great student"
+def print_footer
+  if @students.count == 1
+    puts "Overall, we have #{@students.count} great student"
   else
-    puts "Overall, we have #{names.count} great students"
+    puts "Overall, we have #{@students.count} great students"
   end
 end
 
-# now I'll call the methods
-#students = input_students
+# now I'll call the method
 interactive_menu
-#print_header
-#print(students)
-#print_footer(students)
